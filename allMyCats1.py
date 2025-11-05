170685c46d8fed2947d44c6254e59a291c72f3bc
dan-gheorghita <79468719+dan-gheorghita@users.noreply.github.com>
2025-11-05 10:32:01 +0200

    Add Python script

---
+++ allMyCats1.py
@@ -0,0 +1,30 @@
+```python
+# Prompt the user to enter the name of the first cat
+print('Enter the name of cat 1:')
+# Store the user's input in the variable catName1
+catName1 = input()
+# Prompt the user to enter the name of the second cat
+print('Enter the name of cat 2:')
+# Store the user's input in the variable catName2
+catName2 = input()
+# Prompt the user to enter the name of the third cat
+print('Enter the name of cat 3:')
+# Store the user's input in the variable catName3
+catName3 = input()
+# Prompt the user to enter the name of the fourth cat
+print('Enter the name of cat 4:')
+# Store the user's input in the variable catName4
+catName4 = input()
+# Prompt the user to enter the name of the fifth cat
+print('Enter the name of cat 5:')
+# Store the user's input in the variable catName5
+catName5 = input()
+# Prompt the user to enter the name of the sixth cat
+print('Enter the name of cat 6:')
+# Store the user's input in the variable catName6
+catName6 = input()
+# Print a message to the user indicating that the cat names are being displayed
+print('The cat names are:')
+# Print the names of all six cats, separated by spaces
+print(catName1 + ' ' + catName2 + ' ' + catName3 + ' ' + catName4 + ' ' + catName5 + ' ' + catName6)
+```
